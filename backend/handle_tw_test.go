88241cd171b30544276e0906e0875c09c5afddfa
Abhishek <bumblebee31304@gmail.com>
2024-06-12 12:43:08 +0530

    feat: add more backend tests
    added tests for setting config, syncing, exporting and completing tasks

--- backend/handle_tw_test.go
+++ backend/handle_tw_test.go
@@ -6,20 +6,11 @@ import (
 )
 
 func TestSetTaskwarriorConfig(t *testing.T) {
-	err := SetTaskwarriorConfig("", "your_secret", "your_container_origin", "your_client_id")
+	err := SetTaskwarriorConfig("./", "encryption_secret", "container_origin", "client_id")
 	if err != nil {
 		t.Errorf("SetTaskwarriorConfig() failed: %v", err)
 	} else {
 		fmt.Println("SetTaskwarriorConfig test passed")
-	}
-}
-
-func TestEditTaskInATaskwarrior(t *testing.T) {
-	err := EditTaskInTaskwarrior("your_task_uuid_here", "description")
-	if err != nil {
-		t.Errorf("EditTaskInTaskwarrior() failed: %v", err)
-	} else {
-		fmt.Println("Edit test passed")
 	}
 }
 
@@ -29,5 +20,50 @@ func TestSyncTasksWithTaskwarrior(t *testing.T) {
 		t.Errorf("SyncTasksWithTaskwarrior() failed: %v", err)
 	} else {
 		fmt.Println("Sync test passed")
+	}
+}
+
+func TestSyncTaskwarrior(t *testing.T) {
+	err := SyncTaskwarrior("./")
+	if err != nil {
+		t.Errorf("SyncTaskwarrior failed: %v", err)
+	} else {
+		fmt.Println("Sync Dir test passed")
+	}
+}
+
+func TestEditTaskInATaskwarrior(t *testing.T) {
+	err := EditTaskInTaskwarrior("task_uuid", "description")
+	if err != nil {
+		t.Errorf("EditTaskInTaskwarrior() failed: %v", err)
+	} else {
+		fmt.Println("Edit test passed")
+	}
+}
+
+func TestExportTasks(t *testing.T) {
+	task, err := ExportTasks("./")
+	if task != nil && err == nil {
+		fmt.Println("Task export test passed")
+	} else {
+		t.Errorf("ExportTasks() failed: %v", err)
+	}
+}
+
+func TestAddTaskToTaskwarrior(t *testing.T) {
+	err := AddTaskToTaskwarrior("email", "encryption_secret", "clientId", "description", "", "H")
+	if err != nil {
+		t.Errorf("AddTaskToTaskwarrior failed: %v", err)
+	} else {
+		fmt.Println("Add task passed")
+	}
+}
+
+func TestCompleteTaskInTaskwarrior(t *testing.T) {
+	err := CompleteTaskInTaskwarrior("email", "encryptionSecret", "client_id", "taskuuid")
+	if err != nil {
+		t.Errorf("CompleteTaskInTaskwarrior failed: %v", err)
+	} else {
+		fmt.Println("Complete task passed")
 	}
 }
