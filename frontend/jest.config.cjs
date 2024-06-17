2b6e62ee810c53e455a115ae7437f560922c8452
Abhishek <bumblebee31304@gmail.com>
2024-06-17 11:22:32 +0530

    test: setup tesing environment for frontend

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!react-toastify)"
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts'
+  ],
+  testMatch: [
+    '**/__tests__/**/*.{ts,tsx}',
+    '**/?(*.)+(spec|test).{ts,tsx}'
+  ],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343]
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: { metaObjectReplacement: { url: 'https://www.url.com' } }
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
