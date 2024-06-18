d4e716ee85dbbfec09df490563fdab047508e681
Abhishek <bumblebee31304@gmail.com>
2024-06-18 13:20:50 +0530

    test: add theme tests and types tests

--- frontend/src/components/utils/theme-mode-toggle.tsx
+++ frontend/src/components/utils/theme-mode-toggle.tsx
@@ -19,8 +19,8 @@ export function ModeToggle() {
           size="icon"
           className="ghost"
         >
-          <Sun className="h-[1.1rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.1rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun data-testid="sun-icon" className="h-[1.1rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon data-testid="moon-icon" className="absolute h-[1.1rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
