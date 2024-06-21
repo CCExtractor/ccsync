3bc47f399524281779877e90380152dfac1ebc8a
Abhishek <bumblebee31304@gmail.com>
2024-06-21 11:39:24 +0530

    feat: filter tasks by status

--- frontend/src/components/HomeComponents/BottomBar/BottomBar.tsx
+++ frontend/src/components/HomeComponents/BottomBar/BottomBar.tsx
@@ -7,14 +7,18 @@ interface BottomBarProps {
   projects: string[];
   selectedProject: string | null;
   setSelectedProject: Dispatch<SetStateAction<string>>;
+
+  status: string[];
+  selectedStatus: string | null;
+  setSelectedStatus: Dispatch<SetStateAction<string>>;
 }
 
-const BottomBar: React.FC<BottomBarProps> = ({ projects, selectedProject, setSelectedProject }) => {
+const BottomBar: React.FC<BottomBarProps> = ({ projects, selectedProject, setSelectedProject, status, selectedStatus, setSelectedStatus }) => {
   return (
     <header className="sm:hidden fixed bottom-0 w-full bg-white border-t-[1px] dark:border-b-slate-700 dark:bg-background shadow-lg flex justify-between items-center p-4 z-40">
       <NavigationMenu className="mx-auto">
-        <div className="flex space-x-3 mr-2">
-          <nav className="md:flex gap-2 justify-center flex-1">
+        <div className="flex space-x-4 mr-2">
+          <nav className="md:flex gap-2 justify-center flex-2">
             <a
               rel="noreferrer noopener"
               href={"#"}
@@ -34,7 +38,7 @@ const BottomBar: React.FC<BottomBarProps> = ({ projects, selectedProject, setSel
           </nav>
         </div>
         <Select value={selectedProject || ""} onValueChange={setSelectedProject}>
-          <SelectTrigger className="w-[120px]">
+          <SelectTrigger className="w-[120px] mr-2">
             <SelectValue placeholder="Select a project" />
           </SelectTrigger>
           <SelectContent>
@@ -44,6 +48,22 @@ const BottomBar: React.FC<BottomBarProps> = ({ projects, selectedProject, setSel
               {projects.map((project) => (
                 <SelectItem key={project} value={project}>
                   {project}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select value={selectedStatus || ""} onValueChange={setSelectedStatus}>
+          <SelectTrigger className="w-[90px]">
+            <SelectValue placeholder="Select a project" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Status</SelectLabel>
+              <SelectItem value="all">All</SelectItem>
+              {status.map((status) => (
+                <SelectItem key={status} value={status}>
+                  {status}
                 </SelectItem>
               ))}
             </SelectGroup>
