4477f2738284b3a159895d971115f08c5e2b8ed2
Abhishek <bumblebee31304@gmail.com>
2024-06-22 12:44:58 +0530

    refactor: fix bottom bar

--- frontend/src/components/HomeComponents/BottomBar/__tests__/bottom-bar-utils.test.ts
+++ frontend/src/components/HomeComponents/BottomBar/__tests__/bottom-bar-utils.test.ts
@@ -1,10 +1,29 @@
-import { RouteProps, routeList } from '../bottom-bar-utils';
+import { BottomBarProps, RouteProps, routeList } from '../bottom-bar-utils';
 
 describe('RouteProps interface', () => {
     it('should have href and label properties', () => {
         const exampleRoute: RouteProps = { href: '#', label: 'Example' };
         expect(exampleRoute).toHaveProperty('href');
         expect(exampleRoute).toHaveProperty('label');
+    });
+});
+
+describe('BottomBarProps interface', () => {
+    it('should have project and status properties', () => {
+        const example: BottomBarProps = {
+            projects: [""],
+            selectedProject: "",
+            setSelectedProject: jest.fn(),
+            status: [""],
+            selectedStatus: "",
+            setSelectedStatus: jest.fn(),
+        };
+        expect(example).toHaveProperty('projects');
+        expect(example).toHaveProperty('selectedProject');
+        expect(example).toHaveProperty('setSelectedProject');
+        expect(example).toHaveProperty('status');
+        expect(example).toHaveProperty('selectedStatus');
+        expect(example).toHaveProperty('setSelectedStatus');
     });
 });
 
