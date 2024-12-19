c18ccce13d786700ee9898d9352dd295edd10fd2
Abhishek <bumblebee31304@gmail.com>
2024-12-19 21:58:56 +0530

    fix: remove logs

--- backend/utils/tw/edit_task.go
+++ backend/utils/tw/edit_task.go
@@ -12,32 +12,27 @@ func EditTaskInTaskwarrior(uuid, description, email, encryptionSecret, taskID st
 	}
 	tempDir, err := os.MkdirTemp("", "taskwarrior-"+email)
 	if err != nil {
-		fmt.Println("Log1")
 		return fmt.Errorf("failed to create temporary directory: %v", err)
 	}
 	defer os.RemoveAll(tempDir)
 
 	origin := os.Getenv("CONTAINER_ORIGIN")
 	if err := SetTaskwarriorConfig(tempDir, encryptionSecret, origin, uuid); err != nil {
-		fmt.Println("Log2")
 		return err
 	}
 
 	if err := SyncTaskwarrior(tempDir); err != nil {
-		fmt.Println("Log3")
 		return err
 	}
 
 	// Escape the double quotes in the description and format it
 	if err := utils.ExecCommand("task", taskID, "modify", description); err != nil {
 		fmt.Println("task " + taskID + " modify " + description)
-		fmt.Println("Log4")
 		return fmt.Errorf("failed to edit task: %v", err)
 	}
 
 	// Sync Taskwarrior again
 	if err := SyncTaskwarrior(tempDir); err != nil {
-		fmt.Println("Log5")
 		return err
 	}
 	return nil
