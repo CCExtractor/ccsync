9dbe6793880ed5616bc5cc4e4ffc0461778b2c5c
Abhishek <bumblebee31304@gmail.com>
2025-07-25 12:15:28 +0530

    feat: add download config button
    Signed-off-by: Abhishek <bumblebee31304@gmail.com>

---
+++ frontend/src/components/HomeComponents/SetupGuide/utils.ts
@@ -0,0 +1,11 @@
+import { Props } from '@/components/utils/types';
+import { url } from '@/components/utils/URLs';
+
+export function exportConfigSetup(props: Props): string {
+  return [
+    'Configure Taskwarrior with these commands, run these commands one block at a time',
+    `task config sync.server.origin ${url.containerOrigin}`,
+    `task config sync.server.client_id ${props.uuid}`,
+    'For more information about how this works, refer to the task-sync(5) manpage for details on how to configure the new sync implementation.',
+  ].join('\n');
+}
