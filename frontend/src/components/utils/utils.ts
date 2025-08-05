63a87ebcaec919530b1152febc4d111bbd96f808
Abhishek <114338679+its-me-abhishek@users.noreply.github.com>
2025-08-05 10:46:48 +0530

    Feature export download (#106)
    * feat: add search functionality
    
    Signed-off-by: Abhishek <bumblebee31304@gmail.com>
    
    * feat: add download config button
    
    Signed-off-by: Abhishek <bumblebee31304@gmail.com>
    
    ---------
    
    Signed-off-by: Abhishek <bumblebee31304@gmail.com>

--- frontend/src/components/utils/utils.ts
+++ frontend/src/components/utils/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Debounce utility
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number
+) {
+  let timeout: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: Parameters<T>) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  } as T;
+}
