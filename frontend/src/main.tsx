c44fce1372950cd5599666fbb027b3aa9ba76ddf
Divyansh Mishra <divyanshmishra@Divyanshs-MacBook-Air-3.local>
2025-10-23 02:06:22 +0530

    t2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { ThemeProvider } from '@/components/utils/theme-provider.tsx;
+import { ThemeProvider } from @/components/utils/theme-provider.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
