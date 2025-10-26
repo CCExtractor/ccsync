6bd8bc417d84da194ad68e68270249c30de9aa1e
prayag78 <dasprayag1234@gmail.com>
2025-10-26 14:45:48 +0530

    fix toggle effect

--- frontend/src/components/HomeComponents/SetupGuide/CopyableCode.tsx
+++ frontend/src/components/HomeComponents/SetupGuide/CopyableCode.tsx
@@ -1,13 +1,21 @@
-import { CopyIcon } from 'lucide-react';
+import { CopyIcon, Eye, EyeOff } from 'lucide-react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { toast } from 'react-toastify';
+import { useState } from 'react';
 
 interface CopyableCodeProps {
   text: string;
   copyText: string;
+  isSensitive?: boolean;
 }
 
-export const CopyableCode = ({ text, copyText }: CopyableCodeProps) => {
+export const CopyableCode = ({
+  text,
+  copyText,
+  isSensitive = false,
+}: CopyableCodeProps) => {
+  const [showSensitive, setShowSensitive] = useState(true);
+
   const handleCopy = (text: string) => {
     toast.success(`${text} copied to clipboard!`, {
       position: 'bottom-left',
@@ -19,14 +27,31 @@ export const CopyableCode = ({ text, copyText }: CopyableCodeProps) => {
       progress: undefined,
     });
   };
+  
+  const createMask = (text: string) => '•'.repeat(text.length);  
 
   return (
     <div className="mt-4 flex items-center gap-2 w-full">
       <div className="bg-gray-900 text-white p-3 sm:p-4 rounded-lg flex-grow overflow-x-auto">
         <code className="text-sm sm:text-base whitespace-nowrap break-all">
-          {text}
+          {isSensitive && !showSensitive ? createMask(text) : text}
         </code>
       </div>
+      {isSensitive && (
+        <button
+          onClick={() => setShowSensitive(!showSensitive)}
+          className="text-white font-bold p-2 rounded-full m-2"
+          aria-label={
+            showSensitive ? 'Hide sensitive value' : 'Show sensitive value'
+          }
+        >
+          {showSensitive ? (
+            <EyeOff className="size-5" />
+          ) : (
+            <Eye className="size-5" />
+          )}
+        </button>
+      )}
       <CopyToClipboard text={copyText} onCopy={() => handleCopy(copyText)}>
         <button className="bg-blue-500 hover:bg-gray-900 text-white font-bold p-3 sm:p-4 rounded flex-shrink-0">
           <CopyIcon className="size-5" />
