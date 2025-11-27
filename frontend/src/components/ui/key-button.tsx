aa111f072f69276b88f1536ac40db417f87535ab
NEERAJ GAGAT <126970195+Neeraj-gagat@users.noreply.github.com>
2025-11-27 18:04:16 +0530

    feat: add keyboard shortcut keys (#214)
    * added keyboard shortcut keys
    
    * added-keys with keyboard-shortcuts
    
    * fixed merge conflicts
    
    * fixed tests

---
+++ frontend/src/components/ui/key-button.tsx
@@ -0,0 +1,11 @@
+export const Key = ({ lable }: { lable: string }) => {
+  return (
+    <img
+      src={`https://key.pics/key/${lable.toUpperCase()}.svg?size=15&color=dark&fontStyle=Bold&fontSize=12`}
+      alt={lable}
+      className="hidden md:inline-block ml-2"
+    >
+      {/* {key} */}
+    </img>
+  );
+};
