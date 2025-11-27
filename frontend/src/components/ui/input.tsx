c041dea89f4331df5b0cfae12eaeee44e5da54e8
Abhishek <bumblebee31304@gmail.com>
2025-11-27 18:15:36 +0530

    test: update snapshots for Navbar, NavbarDesktop, BottomBar, SetupGuide

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 
 import { cn } from '@/components/utils/utils';
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
 }
 
