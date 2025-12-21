a504049fe77e96aca18b44052d6979adc20d48ca
Abhishek <114338679+its-me-abhishek@users.noreply.github.com>
2025-12-21 12:37:14 +0530

    refactor: adjust grid layout for better responsiveness and fix the datepicker (#285)

--- frontend/src/components/ui/date-picker.tsx
+++ frontend/src/components/ui/date-picker.tsx
@@ -23,7 +23,7 @@ export function DatePicker({
   className,
 }: DatePickerProps) {
   return (
-    <Popover>
+    <Popover modal={true}>
       <PopoverTrigger asChild>
         <Button
           variant={'outline'}
