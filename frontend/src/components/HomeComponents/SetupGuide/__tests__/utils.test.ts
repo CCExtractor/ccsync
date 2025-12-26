e42682627eeceb9059aa4b48354289b959ebc1e0
NEERAJ GAGAT <126970195+Neeraj-gagat@users.noreply.github.com>
2025-12-26 20:04:26 +0530

    test(setupguide): add tests for for utils.ts #312

---
+++ frontend/src/components/HomeComponents/SetupGuide/__tests__/utils.test.ts
@@ -0,0 +1,39 @@
+import { url } from '@/components/utils/URLs';
+import { exportConfigSetup } from '../utils';
+
+describe('exportConfigSetup', () => {
+  test('generate correct configuration setup', () => {
+    const props = {
+      encryption_secret: 'encryptionSecret',
+      uuid: 'clients-uuid',
+    } as any;
+
+    const result = exportConfigSetup(props);
+
+    expect(result).toContain(
+      'Configure Taskwarrior with these commands, run these commands one block at a time'
+    );
+
+    expect(result).toContain(
+      `task config sync.encryption_secret ${props.encryption_secret}`
+    );
+
+    expect(result).toContain(
+      `task config sync.server.origin ${url.containerOrigin}`
+    );
+
+    expect(result).toContain(`task config sync.server.client_id ${props.uuid}`);
+  });
+
+  test('returns string seprated with newline', () => {
+    const props = {
+      encryption_secret: 'encryptionSecret',
+      uuid: 'clients-uuid',
+    } as any;
+
+    const result = exportConfigSetup(props);
+
+    const lines = result.split('\n');
+    expect(lines.length).toBeGreaterThan(1);
+  });
+});
