f6e808eb3516f2a9b874d8a8cf87993e78b12c97
greedy-wudpeckr <mudituiet@gmail.com>
2026-02-05 03:28:24 +0530

    added tests for handlelogoClick
    Signed-off-by: greedy-wudpeckr <mudituiet@gmail.com>

--- frontend/src/components/HomeComponents/Footer/__tests__/Footer.test.tsx
+++ frontend/src/components/HomeComponents/Footer/__tests__/Footer.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Footer } from '../Footer';
+import * as utils from '@/components/utils/utils';
 
 jest.mock('../../../../assets/logo.png', () => 'logo-path');
 jest.mock('../../../../assets/logo_light.png', () => 'logo-light-path');
@@ -27,5 +28,23 @@ describe('Footer component using Snapshot', () => {
   test('renders correctly', () => {
     const { asFragment } = render(<Footer />);
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('Footer logo click handler', () => {
+  test('should call handleLogoClick when logo link is clicked', () => {
+    const handleLogoClickSpy = jest.spyOn(utils, 'handleLogoClick');
+    render(<Footer />);
+
+    const logoLinks = screen.getAllByRole('link');
+    const logoLink = logoLinks.find(
+      (link) => link.getAttribute('href') === '#'
+    );
+
+    expect(logoLink).toBeDefined();
+    fireEvent.click(logoLink!);
+    expect(handleLogoClickSpy).toHaveBeenCalledTimes(1);
+
+    handleLogoClickSpy.mockRestore();
   });
 });
